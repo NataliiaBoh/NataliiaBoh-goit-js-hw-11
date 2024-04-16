267d28df6dc9e08d23d381b9077a0d24a2929e5b
NataliiaBoh <homesweethome160126@gmail.com>
2024-04-16 20:08:25 +0300

    add js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,24 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 
-fetch("https://pixabay.com/api/")
+export function searchImages(value) {
+    const BASE_URL = `https://pixabay.com/api/`;
+    const API_KEY = "43313350-287c14b2000f5e13b9dad3a59";
+
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: value,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true
+    })
+        
+    return fetch(`${BASE_URL}?${params}`)
+    .then(recponse => {
+        if(!recponse.ok) {
+            throw new Error(recponse.statusText)
+        }
+      
+        return recponse.json();
+    } );
+}
+
+       
---
+++ src/main.js
@@ -0,0 +1,66 @@
+import { createMarcup } from "./js/render-functions";
+import { searchImages } from "./js/pixabay-api";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const formEl = document.querySelector(".js-search-form");
+const imagesList = document.querySelector(".js-images-list");
+const loaderEl = document.querySelector(".loader");
+
+loaderEl.style.borderColor = 'white';
+loaderEl.style.borderBottomColor = 'transparent';
+
+const lightbox = new SimpleLightbox('.js-images-list a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+
+formEl.addEventListener('submit', handleSubmit);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    imagesList.innerHTML = '';
+    loaderEl.style.borderColor = 'black';
+    loaderEl.style.borderBottomColor = 'transparent';
+
+  const searchInput = event.currentTarget.querySelector('.search-input').value;
+
+       searchImages(searchInput)
+       .then(data => {
+        loaderEl.style.borderColor = 'white';
+loaderEl.style.borderBottomColor = 'transparent';
+        
+        if (!data.hits.length) {
+            iziToast.error({title: 'Error', messege: 'Sorry, there are no images matching your search query. Please try again!',})
+        }
+        return data;
+    })
+      
+        .then (data => {imagesList.innerHTML = ('beforeend', createMarcup(data.hits))
+
+        lightbox.refresh();
+       searchInput = '';
+    })
+      
+    
+
+    .catch(error => {
+      loaderEl.style.display = 'none';
+      if(error.length != undefined) {
+        iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+      }
+      
+    });
+}
